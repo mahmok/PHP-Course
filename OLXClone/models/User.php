7997c69eacac38f81ef8b5115358edd3bac5fb87
Mahmoud Mokhtar <mahmok@gmail.com>
2019-07-19 11:39:02 +0200

    OLX

---
+++ OLXClone/models/User.php
@@ -0,0 +1,57 @@
+<?php 
+
+require("./Connection.php");
+
+class User
+{
+
+    static function login($email, $password)
+    {
+        $conn = new Connection();
+        $conn->createConnection();
+
+        $result = $conn->executeQuery("SELECT * FROM users WHERE email = '$email'");
+
+        $user = mysqli_fetch_object($result);
+
+        $conn->closeConnection();
+        if($user == null)
+        {
+            return $user;
+        }
+
+        if($user->password == $password)
+        {
+            return $user;
+        }
+
+        return null;
+
+
+    }
+
+    static function signup($name, $password, $mobile, $email)
+    {
+        $conn = new Connection();
+        $conn->createConnection();
+
+        $result = $conn->executeQuery("INSERT INTO users (name, email, password, mobile) VALUES ('$name', '$email', '$password', '$mobile') ");
+
+        $lastId = -1;
+        if($result)
+        {
+            $lastId = $conn->getLastInsertedId();
+        }
+        
+
+        return $lastId;
+
+
+    }
+
+}
+
+
+
+
+?>
